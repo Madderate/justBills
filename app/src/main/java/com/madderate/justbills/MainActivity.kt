0ce1ad9064ce036f8cf717e94a06647cbb164155
madderate <jianzengzhi.liao@duitang.com>
2021-01-03 23:38:00 +0800

    ➡️  adjust main activity & use view binding

--- app/src/main/java/com/madderate/justbills/MainActivity.kt
+++ app/src/main/java/com/madderate/justbills/MainActivity.kt
@@ -2,10 +2,22 @@ package com.madderate.justbills
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.madderate.justbills.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        mainBinding.mainToolbar.title = "justBills"
+        setSupportActionBar(mainBinding.mainToolbar)
     }
 }
